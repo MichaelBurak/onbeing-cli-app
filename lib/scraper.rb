551b2499cac2c4ba676cadc32b15e477195209e1
Michael Burak <michaelrburak@gmail.com>
2017-01-05 17:41:05 +0000

    scraper now scrapes text of each episode name

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,13 @@
 class Philosophy_podcast::Scraper
 
-  def self.scrape_episode_list
-    doc = Nokogiri::HTML(open('http://www.onbeing.org/programs/2016'))
-    doc
+  def self.scrape #main scraper
+    Nokogiri::HTML(open('http://www.onbeing.org/programs/2016')).xpath("//span[@class='field-content']/a")
+  end
+
+  def self.scrape_episode_list #scrapes text of each podcast episode name
+    self.scrape.xpath("//span[@class='field-content']/a").each_with_index do |pod, i|
+      puts "#{i}... #{pod.text}"
+    end
   end
 
 end
