f2bb571c762408cd7f6daa64d715fdf6ab4c1984
Michael Burak <michaelrburak@gmail.com>
2017-01-05 17:56:17 +0000

    stubbed out podcast and cli classes

---
+++ lib/cli.rb
@@ -0,0 +1,17 @@
+class CLI
+
+  def call
+    puts "Welcome to the 2016 episode archive of On Being. Do you want to see a list of episodes? y or n"
+    input = gets.strip
+    case input
+    when "y"
+    Philosophy_podcast::Scraper.scrape_episode_list
+    puts "Would you like to see more detail on an episode? Type the episode's number."
+    #display Podcast class's attributes by index, input = index 
+    when "n"
+      "Goodbye!"
+      break
+    end
+  end
+
+end
---
+++ lib/podcast.rb
@@ -0,0 +1,3 @@
+class Philosophy_podcast::Podcast
+  attr_accessor :guest, :description, :date, :title
+end
