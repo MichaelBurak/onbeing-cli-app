0ba688e8cf718eeedff2725c797233047044a71c
Michael Burak <michaelrburak@gmail.com>
2017-01-06 20:55:05 +0000

    refining CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,15 +5,23 @@ class Philosophy_podcast::CLI
     input = gets.strip
     case input
     when "y"
+    Philosophy_podcast::Podcast.add_podcasts
     puts "do you want to see 1-10, 2,20, etc."
-    #input = gets.strip.to_i
-    #Philosophy_podcast::Podcast method for displaying episode list(input), refer to world's best restaurant
+    i = gets.strip.to_i
+    print_list(i)
     puts "Would you like to see more detail on an episode? Type the episode's number."
     #display Podcast @@all but by index with input = index - 1
     #add in ability to go back to the list, exit
     #add in stuff for non-applicable input
     when "n"
       "Goodbye!"
+    end
+  end
+
+  def print_list(from_number)
+    puts "Episodes #{from_number} - #{from_number +9}"
+    puts Philosophy_podcast::Podcast.all[from_number-1, 10].each_with_index(from_number) do |podcast, index|
+      puts "#{index}. #{podcast.title}"
     end
   end
 
