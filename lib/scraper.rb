f2fb17361477423dc21aede6fd0202a9a91fdc2c
Michael Burak <michaelrburak@gmail.com>
2017-01-06 21:20:47 +0000

    removed binding so bin entry into program works

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,5 @@ class Philosophy_podcast::Scraper
   def self.scrape #main scraper
     Nokogiri::HTML(open('http://www.onbeing.org/programs/2016'))#.xpath("//span[@class='field-content']/a")
   end
-
-  binding.pry
-
+  
 end
