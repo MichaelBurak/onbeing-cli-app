0d52cbcc2824be9398536f08b4eadf3fcc95a7a4
Michael Burak <michaelrburak@gmail.com>
2017-01-07 13:55:23 +0000

    trying to add loop to cli, curently infinite

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,9 +3,12 @@ class Philosophy_podcast::CLI
   def call
     puts "Welcome to the 2016 episode archive of On Being. Do you want to see a list of episodes? y or n"
     input = gets.strip
-    case input
-    when "y"
+    if input == "n" then
+      puts "Goodbye!"
+    elsif input == "y"
     Philosophy_podcast::Podcast.add_podcasts
+    while input || i3 != "n"
+      begin
     puts "do you want to see 1-10, 2,20, etc."
     i = gets.strip.to_i
     print_list(i)
@@ -13,10 +16,14 @@ class Philosophy_podcast::CLI
     i2 = gets.strip.to_i
     print_episode(i2)
     puts "Would you like to go back to the list? y or n"
-    #how to loop here using i2's input to go back to the correct list?
+    i3 = gets.strip
+    if i3 == "y" then next
+    elsif i3 == "n" then
+      puts "Goodbye"
+    end while i3 || input == "n"
+    end #how to loop here using i2's input to go back to the correct list?
     #add in stuff for non-applicable input
-    when "n"
-      "Goodbye!"
+  end
     end
   end
 
