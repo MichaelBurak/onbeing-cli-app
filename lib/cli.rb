bbe0dacc18bcc8c8cf59406cbaece276c2dee49b
Michael Burak <michaelrburak@gmail.com>
2017-01-07 14:22:41 +0000

    fixed inifnite loop

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,27 +1,26 @@
 class Philosophy_podcast::CLI
 
   def call
-    puts "Welcome to the 2016 episode archive of On Being. Do you want to see a list of episodes? y or n"
+    puts "Welcome to the 2016 episode archive of On Being. Do you want to see a list of episodes? Enter y or n."
     input = gets.strip
     if input == "n" then
       puts "Goodbye!"
     elsif input == "y"
     Philosophy_podcast::Podcast.add_podcasts
-    while input || i3 != "n"
+    #while input || i3 != "n"
       begin
-    puts "do you want to see 1-10, 2,20, etc."
+    puts "Do you want to see episodes 1-10, 5-14, etc.?"
     i = gets.strip.to_i
     print_list(i)
     puts "Would you like to see more detail on an episode? Type the episode's number."
     i2 = gets.strip.to_i
     print_episode(i2)
-    puts "Would you like to go back to the list? y or n"
+    puts "Would you like to go back to the list? y or n? n will quit the program."
     i3 = gets.strip
     if i3 == "y" then next
-    elsif i3 == "n" then
-      puts "Goodbye"
-    end while i3 || input == "n"
-    end #how to loop here using i2's input to go back to the correct list?
+    end until i3 == "n"
+
+     #how to make this not an infinite loop back to 'do you want to see'
     #add in stuff for non-applicable input
   end
     end
